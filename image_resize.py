7f39072b4c4b8dc235fce8ab43a8b65e328169e9
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-05-18 21:20:40 +0300

    Added parsing of command line arguments

--- image_resize.py
+++ image_resize.py
@@ -1,9 +1,54 @@
+import argparse
+
+
 def resize_image(path_to_original, path_to_result):
     pass
 
 
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'filename',
+        help='a image file for resizing (should be either JPEG or PNG)',
+        type=str,
+    )
+
+    parser.add_argument(
+        '--width',
+        help='a width of output image file (in pixels)',
+        default=0,
+        type=int,
+    )
+
+    parser.add_argument(
+        '--height',
+        help='a height of output image file (in pixels)',
+        default=0,
+        type=int,
+    )
+
+    parser.add_argument(
+        '--scale',
+        help='a scaling coefficient of output image file (should be > 0)',
+        default=0.0,
+        type=float,
+    )
+
+    parser.add_argument(
+        '--output',
+        help='a path for saving of output image file',
+        default='',
+        type=str,
+    )
+
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
+
+
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
 
 
 if __name__ == '__main__':
